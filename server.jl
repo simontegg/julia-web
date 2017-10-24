a228a7ab3ff33de7c4d7f8ee27dbfc428273c357
simontegg <simon@enspiral.com>
2017-10-24 13:35:45 +1300

    routing

--- server.jl
+++ server.jl
@@ -1,6 +1,5 @@
 using HttpServer, Lazy
 
-
 include("./pages/UserBiometrics.jl")
 
 function stack(handlers)
@@ -8,54 +7,44 @@ function stack(handlers)
 
   function compose(index, func)
     if index < length(reversed)
-      next = index + 1
-      child = reversed[next]
+      prev = func
+      next_i = index + 1
+      child = reversed[next_i]
 
-
-
-      destructure = function (args)
-        req, res = args
-        @show typeof(res)
-
-        func(req, res)
+      function next(args)
+        if isa(args, Tuple)
+          req, res = args
+          func(req, res)
+        else
+          return args
+        end
       end
 
+      composed = next ∘ child
 
-
-      composed = destructure ∘ child
-
-      return compose(next, composed)
+      return compose(next_i, composed)
     else
       return func
     end
   end
 
-
   return function(req, res)
-    
     index = 1
     app = compose(index, reversed[index])
     return app(req, res)
   end
 end
 
-
-
 function hello(req, res)
-  # req, res = args
-
   @show "hello"
-  # @show args
 
   return req, res
 end
 
 function hi(req, res)
-  # req, res = args
   @show "hi"
-  @show req[:path]
 
-  req[:resource] == "/" ? (req, "hi") : (req, res)
+  req[:resource] == "/" ? "hi" : (req, res)
 end
 
 splitpath(p::AbstractString) = split(p, "/", keep=false)
@@ -76,45 +65,27 @@ function get_path(req, res)
 end
 
 function route_test(req, res)
+  @show "route_test"
   path = req[:path]
 
-  @show "route_test"
-  @show length(path)
-
-
   if (length(path) == 1 && path[1] == "test")
-    return req, "you routed to /test"
+    @show "route_test_return"
+    return "you routed to /test"
   else
     return req, res
   end
 end
 
 function sub_route_test(req, res)
-  path = req[:path]
   @show "sub_route_test"
-  @show res
+  path = req[:path]
 
   if (length(path) > 0 && path[1] == "test")
-    return req, "you routed to /test/something"
+    return "you routed to /test/something"
   else
     return req, res
   end
 end
-
-
-# function routes(args)
-#   req, res = args
-
-
-
-
-
-#   req.resource = "/test" ? route(args) : args
-# end
-
-# function route()
-
-# end
 
 function unfold(iter)
   result = []
@@ -134,13 +105,7 @@ function main(args...)
   handlers = unfold(args)
   app = stack(handlers)
 
-  function destructure(req, res)
-    request, response = app(req, res)
-
-    return response
-  end
-
-  handler = HttpHandler(destructure)
+  handler = HttpHandler(app)
   handler.events["error"]  = (client, error) -> println(error)
   handler.events["listen"] = (port)          -> println("Listening on $port...")
 
@@ -152,8 +117,6 @@ test_routes = [route_test, sub_route_test]
 server = main(
   todict,
   get_path,
-  # route_test,
-  # sub_route_test,
   stack(test_routes),
   hello,
   hi
